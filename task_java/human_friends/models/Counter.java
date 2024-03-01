f6aa519d9140f362053b6bb802e722cb1bccca5f
Evgenia Lizunova <119034453+EvgNicL@users.noreply.github.com>
2024-03-01 12:23:23 +0400

    class Counter added

---
+++ task_java/human_friends/models/Counter.java
@@ -0,0 +1,20 @@
+package human_friends.models;
+
+import java.util.ArrayList;
+
+public class Counter {
+    public Counter() {
+    }
+    public Integer counter(ArrayList<Object> list){
+//        try {
+//            Object lastLine = list.get(list.size()-1);
+//            for (int i = 0; true; i++) {
+//                return (Integer) lastLine[0];
+//            }
+//
+//        } catch (RuntimeException e) {
+//            System.out.println("ошибка определения последней записи");
+//        }
+        return 0;
+    }
+}
