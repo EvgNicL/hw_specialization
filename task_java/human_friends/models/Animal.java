3e4184ccb5648e158a4dcda2699f5be201282b8b
Evgenia Lizunova <119034453+EvgNicL@users.noreply.github.com>
2024-03-02 22:10:07 +0400

    Update Animal.java var done public

--- task_java/human_friends/models/Animal.java
+++ task_java/human_friends/models/Animal.java
@@ -2,9 +2,9 @@ package human_friends.models;
 import java.util.Arrays;
 
 public class Animal extends Table{
-    String birthday;
-    String typeName;
-    String commands;
+    public String birthday;
+    public String typeName;
+    public String commands;
     public Animal(Integer id, String typeName, String name, String birthday, String commands) {
         super(id, name);
         this.typeName = typeName;
